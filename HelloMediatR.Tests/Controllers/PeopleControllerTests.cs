56db3a3127081f3a1c1e55dbb7f68aacb28cad6f
JGMelon22 <jgvasconcellos22@gmail.com>
2024-06-29 13:37:13 -0300

    tests: developing person controller tests

--- HelloMediatR.Tests/Controllers/PeopleControllerTests.cs
+++ HelloMediatR.Tests/Controllers/PeopleControllerTests.cs
@@ -1,3 +1,4 @@
+using ApiMediaRDemo.Application.Queries;
 using ApiMediaRDemo.DTOs;
 using ApiMediaRDemo.Models;
 using ApiMediatRDemo.Application.Commands;
@@ -7,6 +8,7 @@ using FluentAssertions;
 using FluentValidation;
 using FluentValidation.Results;
 using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HelloMediatR.Tests.Controllers;
@@ -64,9 +66,113 @@ public class PeopleControllerTests
     public async Task PeopleController_GetAllPeopleAsync_ReturnsPeople()
     {
         // Arrange
+        var peopleResult = new List<PersonResult>
+        {
+            new() { Id = Guid.NewGuid(), FullName = "Laura Martinez", Age = 21 },
+            new() { Id = Guid.NewGuid(), FullName = "Maria Gonzalez", Age = 20 },
+            new() { Id = Guid.NewGuid(), FullName = "Isabella Hernandez", Age = 22 }
+        };
+
+        var serviceResponse = new ServiceResponse<List<PersonResult>>
+        {
+            Data = peopleResult
+        };
+
+        A.CallTo(() => _mediator.Send(A<GetPeopleQuery>._, default))
+            .Returns(Task.FromResult(serviceResponse));
 
         // Act
+        var result = await _controller.GetAllPeopleAsync();
 
         // Assert
+        result.Should().NotBeNull();
+        Assert.IsType<OkObjectResult>(result);
+        serviceResponse.Data.Count().Should().Be(3);
+        result.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Fact]
+    public async Task PeopleController_GetPersonByIdAsync_ReturnsPerson()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        var personResult = new PersonResult
+        {
+            Id = id,
+            FullName = "Ana Beatriz",
+            Age = 13
+        };
+
+        var serviceResponse = new ServiceResponse<PersonResult>
+        {
+            Data = personResult
+        };
+
+        A.CallTo(() => _mediator.Send(A<GetPersonByIdQuery>.That.Matches(x => x.Id == id), default))
+            .Returns(Task.FromResult(serviceResponse));
+
+        // Act 
+        var result = await _controller.GetPersonByIdAsync(id);
+
+        // Assert
+        result.Should().NotBeNull();
+        Assert.IsType<OkObjectResult>(result);
+        serviceResponse.Data.Should().Be(personResult);
+        result.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Fact]
+    public async Task PeopleController_UpdatePersonAsync_ReturnsPerson()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        var updatedPerson = new PersonInput("Pedro Enrique", 14);
+        var validationResult = new ValidationResult();
+        var personResult = new PersonResult
+        {
+            Id = id,
+            FullName = "Pedro Enrique",
+            Age = 14
+        };
+
+        var serviceResponse = new ServiceResponse<PersonResult>
+        {
+            Data = personResult
+        };
+
+        A.CallTo(() => _validator.ValidateAsync(updatedPerson, default))
+            .Returns(Task.FromResult(validationResult));
+
+        A.CallTo(() => _mediator.Send(A<UpdatePersonCommand>.That.Matches(x => x.Id == id && x.UpdatedPerson == updatedPerson), default))
+            .Returns(Task.FromResult(serviceResponse));
+
+        // Act
+        var result = await _controller.UpdatePersonAsync(id, updatedPerson);
+
+        // Assert
+        result.Should().NotBeNull();
+        Assert.IsType<OkObjectResult>(result);
+        serviceResponse.Data.Should().Be(personResult);
+        result.Should().BeOfType<OkObjectResult>();
+    }
+
+    [Fact]
+    public async Task PeopleController_RemovePersonAsync_ReturnsPerson()
+    {
+        // Arrange
+        Guid id = Guid.NewGuid();
+        var serviceResponse = new ServiceResponse<bool>();
+
+        A.CallTo(() => _mediator.Send(A<RemovePersonCommand>.That.Matches(x => x.Id == id), default))
+            .Returns(Task.FromResult(serviceResponse));
+
+        // Act
+        var result = await _controller.RemovePersonAsync(id);
+
+        // Assert
+        result.Should().NotBeNull();
+        Assert.IsType<NoContentResult>(result);
+        serviceResponse.Success.Should().BeTrue();
+        result.Should().BeOfType<NoContentResult>();
     }
 }
